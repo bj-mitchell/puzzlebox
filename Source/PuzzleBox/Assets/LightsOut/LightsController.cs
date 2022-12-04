488d204d2dbda823da6dbad8ebb0876e282d95eb
Robert Mitchell <robert.jeff.mitchell@gmail.com>
2022-12-04 13:15:33 -0500

    Sanity commit.

--- Source/PuzzleBox/Assets/LightsOut/LightsController.cs
+++ Source/PuzzleBox/Assets/LightsOut/LightsController.cs
@@ -4,11 +4,14 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
-using static UnityEditor.ObjectChangeEventStream;
+using Random = UnityEngine.Random;
+//using static UnityEditor.ObjectChangeEventStream;
 
 public class LightsController : MonoBehaviour
 {
     [SerializeField] private Light [] lights;
+    [SerializeField] private int matrixWidth = 4;
+    [SerializeField] private int matrixHeight = 4;
     [SerializeField] private string scheme = "ws";
     [SerializeField] private string host = "172.16.1.10";
     [SerializeField] private int port = 7777;
@@ -16,11 +19,12 @@ public class LightsController : MonoBehaviour
     private TcpConfig tcpConfig;
     private SimpleWebClient client;
     private UriBuilder builder;
+    private Light[,] matrix;
 
     // Start is called before the first frame update
     void Start()
     {
-        tcpConfig = new TcpConfig(noDelay: false, sendTimeout: 5000, receiveTimeout: 20000);
+        tcpConfig = new TcpConfig(noDelay: false, sendTimeout: 10000, receiveTimeout: 20000);
         client = SimpleWebClient.Create(ushort.MaxValue, 5000, tcpConfig);
 
         client.onConnect += Client_onConnect;
@@ -37,7 +41,80 @@ public class LightsController : MonoBehaviour
 
         client.Connect(builder.Uri);
 
+        SetupLights();
 
+        //RandomizeLights();
+    }
+
+    private void SetupLights()
+    {
+        matrix = new Light[matrixWidth, matrixHeight];
+
+        int count = 0;
+        for (int w = 0; w < matrixWidth; w++)
+        {
+            for (int h = 0; h < matrixHeight; h++)
+            {
+                matrix[w, h] = lights[count++];
+            }
+        }
+    }
+
+    private void RandomizeLights()
+    {
+        
+        Random.Range(1, 10);
+
+        for (int i = 0; i < Random.Range(3, 10); i++)
+        {
+            int x = Random.Range(0, matrixWidth);
+            int y = Random.Range(0, matrixHeight);
+            matrix[x,y].State = !matrix[x, y].State;
+        }
+
+        if (CheckAllLightsOn())
+        {
+            int index = Random.Range(0, lights.Length);
+            lights[index].State = true;
+        }
+    }
+
+    private bool CheckAllLightsOn()
+    {
+        foreach (var light in lights)
+        {
+            if (light.State)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void LightSpreadHandler(int row, int col)
+    {
+        matrix[row,col].State = !matrix[row, col].State;
+        
+        if (row > 0) //Top
+        {
+            matrix[row - 1, col].State = !matrix[row - 1, col].State;
+        }
+
+        if (row < matrixHeight - 1) //Bottom
+        {
+            matrix[row + 1, col].State = !matrix[row + 1, col].State;
+        }
+
+        if (col >= 0 && col < matrixWidth - 1) // Right
+        {
+            matrix[row, col + 1].State = !matrix[row, col + 1].State;
+        }
+
+        if (col > 0 && col < matrixWidth) // Left
+        {
+            matrix[row, col - 1].State = !matrix[row, col - 1].State;
+        }
     }
 
     private void Client_onError(Exception obj)
@@ -51,9 +128,28 @@ public class LightsController : MonoBehaviour
         string message = System.Text.Encoding.Default.GetString(obj);
         Debug.Log("onData: " + message);
 
-        var light = Array.Find(lights, element => element.Character == message[0]);
+        //var light = Array.Find(lights, element => element.Character == message[0]);
+        var index = Array.FindIndex(lights, element => element.Character == message[0]);
 
-        light.State = !light.State;
+        //Debug.Log(index);
+
+
+        //Debug.Log("mod=" + (index % matrixWidth) + "; " + "col=" + (index / matrixWidth));
+        int col = (index / matrixWidth);
+        int row = (index % matrixWidth);
+
+        LightSpreadHandler(col, row);
+        //Debug.Log("x=" + x + "; " + "y=" + y);
+        /*if (index < matrixWidth)
+        {
+            Debug.Log("x=" + index + "; y=" + 0);
+        } else {
+            Debug.Log("mod=" + (index % matrixWidth) + "; " + "col=" + (index / matrixWidth));
+
+        }*/
+        // if greater than matrixwidth then index mod matrixwidth and index divided by matrix width;  divided will be row, mod will be column
+
+        //light.State = !light.State;
 
         //throw new NotImplementedException();
     }
