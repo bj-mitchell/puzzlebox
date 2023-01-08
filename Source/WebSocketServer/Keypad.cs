25b8b69ddb1eec508696b8097c510a1404c3a360
Robert Mitchell <robert.jeff.mitchell@gmail.com>
2023-01-08 10:27:05 -0500

    Working on adding interrupt.

--- Source/WebSocketServer/Keypad.cs
+++ Source/WebSocketServer/Keypad.cs
@@ -3,25 +3,30 @@ using System.Device.I2c;
 
 namespace WebSocketServer
 {
+    public delegate void KeypadNotify();
+
     public class Keypad
     {
-        private readonly I2cDevice? keypad;
+        //public event KeypadNotify OnKeypadButtonPress;
+
+        private I2cDevice? keypad = null;
         private readonly GpioController gpio;
         private const int I2C_BUS = 1;
         private const int I2C_ADDRESS = 0x2a;
         private const int INTERRUPT_PIN = 17;
         private const int RESET_PIN = 18;
-
+        
         public Keypad(GpioController gpio)
         {
             this.gpio = gpio;
             Setup();
             Reset();
+            gpio.RegisterCallbackForPinValueChangedEvent(INTERRUPT_PIN, PinEventTypes.Rising, OnInterruptEvent);
         }
-
+        
         private void Reset()
         {
-            Console.WriteLine("Resetting keypad...");
+            Console.WriteLine("Resetting keypad controller...");
             gpio.Write(RESET_PIN, 1);
             Thread.Sleep(200);
             gpio.Write(RESET_PIN, 0);
@@ -29,9 +34,15 @@ namespace WebSocketServer
 
         private void Setup()
         {
-            I2cDevice keypad = I2cDevice.Create(new I2cConnectionSettings(I2C_BUS, I2C_ADDRESS));
-            gpio.OpenPin(INTERRUPT_PIN, System.Device.Gpio.PinMode.Input);
-            gpio.OpenPin(RESET_PIN, System.Device.Gpio.PinMode.Output);
+            keypad = I2cDevice.Create(new I2cConnectionSettings(I2C_BUS, I2C_ADDRESS));
+            gpio.OpenPin(INTERRUPT_PIN, PinMode.Input);
+            gpio.OpenPin(RESET_PIN, PinMode.Output);
         }
+
+        private static void OnInterruptEvent(object sender, PinValueChangedEventArgs pinValueChangedEventArgs)
+        {
+            Console.WriteLine("GOT A INTERRUPT EVENT");
+        }
+
     }
 }
